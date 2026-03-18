1580ceaed0aba1941f2cdce6544b6743f10e38bf
Junko Enoshima <156222018+2dChan@users.noreply.github.com>
2026-03-18 18:12:33 +0300

    feat: add dashboard backend module

---
+++ dashboard/backend/cmd/test/main.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2026 Andrey Kriulin
+// Licensed under the GNU Affero General Public License v3.0 or later.
+// See the LICENSE file in the project root for the full license text.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
