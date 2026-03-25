2f6a636060e522f55fdb27abf8cca9ebd10ed0ae
Junko Enoshima <156222018+2dChan@users.noreply.github.com>
2026-03-25 08:32:47 +0300

    feat(bots): add rebalance strategy and cmd/rebalance entrypoint

---
+++ bots/cmd/rebalance/main.go
@@ -0,0 +1,55 @@
+// Copyright (C) 2026 Andrey Kriulin
+// Licensed under the GNU Affero General Public License v3.0 or later.
+// See the LICENSE file in the project root for the full license text.
+
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/2dChan/trade-engine/adapters/bcs"
+	"github.com/2dChan/trade-engine/bots/internal/botkit"
+	"github.com/2dChan/trade-engine/bots/internal/logging"
+	"github.com/2dChan/trade-engine/bots/internal/strategies/rebalance"
+)
+
+func run(logger *slog.Logger) error {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	token, err := cfg.Token()
+	if err != nil {
+		return err
+	}
+	// TODO: Choose broker via config.
+	brk, err := bcs.NewAdapter(ctx, token)
+	if err != nil {
+		return err
+	}
+	prx, err := botkit.NewProxy(ctx, brk, cfg.AccountID)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Choose strategy via config.
+	strategy := rebalance.NewStrategy()
+
+	slogger := logger.With("broker", prx.Name(), "strategy", strategy.Name())
+	err = strategy.Run(ctx, slogger, prx)
+	stop()
+
+	return err
+}
+
+func main() {
+	logger := logging.NewLogger()
+
+	if err := run(logger); err != nil {
+		logger.Error("bot failed", "error", err)
+		os.Exit(1)
+	}
+}
