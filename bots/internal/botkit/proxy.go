0ccd9c442abdacf022a78f39e7c43cbff99c0f28
Junko Enoshima <156222018+2dChan@users.noreply.github.com>
2026-03-25 10:37:40 +0300

    refactor(bots): replace AccountID() with Account() in Proxy

--- bots/internal/botkit/proxy.go
+++ bots/internal/botkit/proxy.go
@@ -13,8 +13,8 @@ import (
 )
 
 type Proxy struct {
-	broker    broker.Broker
-	accountID string
+	broker  broker.Broker
+	account trade.Account
 }
 
 func NewProxy(ctx context.Context, b broker.Broker, accountID string) (Proxy, error) {
@@ -23,9 +23,11 @@ func NewProxy(ctx context.Context, b broker.Broker, accountID string) (Proxy, er
 		return Proxy{}, fmt.Errorf("botkit: new proxy: %w", err)
 	}
 
+	var account trade.Account
 	ok := false
 	for _, a := range accounts {
 		if a.ID == accountID {
+			account = a
 			ok = true
 			break
 		}
@@ -34,35 +36,35 @@ func NewProxy(ctx context.Context, b broker.Broker, accountID string) (Proxy, er
 		return Proxy{}, fmt.Errorf("botkit: new proxy: account %q: %w", accountID, broker.ErrInvalidAccountID)
 	}
 
-	return Proxy{broker: b, accountID: accountID}, nil
+	return Proxy{broker: b, account: account}, nil
 }
 
 func (p Proxy) Name() string {
 	return p.broker.Name()
 }
 
-func (p Proxy) AccountID() string {
-	return p.accountID
+func (p Proxy) Account() trade.Account {
+	return p.account
 }
 
 func (p Proxy) Portfolio(ctx context.Context) (trade.Portfolio, error) {
-	return p.broker.Portfolio(ctx, p.accountID)
+	return p.broker.Portfolio(ctx, p.account.ID)
 }
 
 func (p Proxy) Orders(ctx context.Context) ([]trade.OrderState, error) {
-	return p.broker.Orders(ctx, p.accountID)
+	return p.broker.Orders(ctx, p.account.ID)
 }
 
 func (p Proxy) OrderState(ctx context.Context, orderID string) (trade.OrderState, error) {
-	return p.broker.OrderState(ctx, p.accountID, orderID)
+	return p.broker.OrderState(ctx, p.account.ID, orderID)
 }
 
 func (p Proxy) PlaceOrder(ctx context.Context, order trade.Order) (string, error) {
-	return p.broker.PlaceOrder(ctx, p.accountID, order)
+	return p.broker.PlaceOrder(ctx, p.account.ID, order)
 }
 
 func (p Proxy) CancelOrder(ctx context.Context, orderID string) error {
-	return p.broker.CancelOrder(ctx, p.accountID, orderID)
+	return p.broker.CancelOrder(ctx, p.account.ID, orderID)
 }
 
 func (p Proxy) InstrumentByTicker(ctx context.Context, ticker string) (trade.Instrument, error) {
