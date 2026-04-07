08a52838d878a65354ba7ef2a4ac2831816b75c9
Junko Enoshima <156222018+2dChan@users.noreply.github.com>
2026-04-07 11:22:30 +0300

    feat(lib): add asset amount and code primitives

---
+++ lib/asset/code.go
@@ -0,0 +1,16 @@
+// Copyright (C) 2026 Andrey Kriulin
+// Licensed under the GNU Affero General Public License v3.0 or later.
+// See the LICENSE file in the project root for the full license text.
+
+package asset
+
+type Code string
+
+const (
+	AssetXXX Code = "XXX"
+	// Fiat
+	AssetRUB Code = "RUB"
+	AssetUSD Code = "USD"
+	AssetEUR Code = "EUR"
+	AssetCNY Code = "CNY"
+)
