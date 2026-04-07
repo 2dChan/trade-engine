d643747a522004cfc695a1d39486fb6c0d2b6547
Junko Enoshima <156222018+2dChan@users.noreply.github.com>
2026-04-07 11:23:06 +0300

    refactor(lib/trade): migrate domain models to asset amounts and split files

---
+++ lib/trade/operation.go
@@ -0,0 +1,24 @@
+// Copyright (C) 2026 Andrey Kriulin
+// Licensed under the GNU Affero General Public License v3.0 or later.
+// See the LICENSE file in the project root for the full license text.
+
+package trade
+
+import (
+	"github.com/2dChan/trade-engine/lib/asset"
+	"github.com/govalues/decimal"
+)
+
+type Position struct {
+	Ticker       string
+	Type         InstrumentType
+	AveragePrice asset.Amount
+	CurrentPrice asset.Amount
+	Quantity     decimal.Decimal
+}
+
+type Portfolio struct {
+	AccountID   string
+	TotalAmount asset.Amount
+	Positions   []Position
+}
